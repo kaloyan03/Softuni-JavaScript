8ad2b63ac9a27b2b14b94800c68fc6a0b485bb52
kaloyan03 <87472356+kaloyan03@users.noreply.github.com>
2021-11-07 23:02:34 +0200

    Delete 05 - Single Page Applications directory

--- 05 - Single Page Applications/01.Forum/app.js
+++
@@ -1,9 +0,0 @@
-import topicPage from "./src/pages/topicsPage.js";
-import commentPage from "./src/pages/commentPage.js";
-
-function setup() {
-    topicPage.initializePage('#post-button', '#cancel-button', '#home-page');
-    commentPage.initializePage('#comment-page');
-}
-
-setup();
